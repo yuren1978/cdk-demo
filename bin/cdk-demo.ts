86e306a3cb03164c921800cad062e8302e6332a5
Jianwei Sun <sun.381@osu.edu>
2021-06-17 21:13:03 -0500

    update packages

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkDemoStack } from '../lib/cdk-demo-stack';
+import { FargateDemoStack } from '../lib/fagate';
 
 const app = new cdk.App();
-new CdkDemoStack(app, 'CdkDemoStack', {
+new FargateDemoStack(app, 'FargateDemoStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
